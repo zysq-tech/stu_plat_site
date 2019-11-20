4e46e94103fbad9839b2cda55a1f27450f63cf15
AllenDK <allendk@foxmail.com>
2019-11-20 14:21:50 +0800

    fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,61 +7,61 @@ import "../styles/about.css"
 import Card from "../components/card"
 
 const AboutPage = () => {
-	const data = useStaticQuery(graphql`
-		query {
-			placeholderImage: file(relativePath: { eq: "images/icon.png" }) {
-				childImageSharp {
-					fluid {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-		}
-	`)
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "images/icon.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
 
-	return (
-		<div>
-			<SEO title='About' />
-			<center>
-				<div className='logo'>
-					<Img fluid={data.placeholderImage.childImageSharp.fluid} />
-				</div>
-			</center>
-			<Card style={{ padding: 15 }}>
-				<center>
-					<p className='greyText'>官方微信</p>
-				</center>
-				<center>
-					<p className='contentText'>wechat id</p>
-				</center>
-			</Card>
-			<Card style={{ padding: 15 }}>
-				<center>
-					<p className='greyText'>官方网站</p>
-				</center>
-				<center>
-					<p className='contentText'>www.zhiyuantech.com</p>
-				</center>
-			</Card>
-			<Card style={{ padding: 15 }}>
-				<center>
-					<p className='greyText'>官方邮箱</p>
-				</center>
-				<center>
-					<p className='contentText'>878545887@qq.com</p>
-				</center>
-			</Card>
-			<center>
-				<Link to='/terms' className='link'>
-					《注册协议》
-				</Link>
-				&
-				<Link to='/policy' className='link'>
-					《隐私协议》
-				</Link>
-			</center>
-		</div>
-	)
+  return (
+    <div>
+      <SEO title="About" />
+      <center>
+        <div className="logo">
+          <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+        </div>
+      </center>
+      <Card style={{ padding: 15 }}>
+        <center>
+          <p className="greyText">官方微信</p>
+        </center>
+        <center>
+          <p className="contentText">wechat id</p>
+        </center>
+      </Card>
+      <Card style={{ padding: 15 }}>
+        <center>
+          <p className="greyText">官方网站</p>
+        </center>
+        <center>
+          <p className="contentText">www.zhiyuantech.net</p>
+        </center>
+      </Card>
+      <Card style={{ padding: 15 }}>
+        <center>
+          <p className="greyText">官方邮箱</p>
+        </center>
+        <center>
+          <p className="contentText">878545887@qq.com</p>
+        </center>
+      </Card>
+      <center>
+        <Link to="/terms" className="link">
+          《注册协议》
+        </Link>
+        &
+        <Link to="/policy" className="link">
+          《隐私协议》
+        </Link>
+      </center>
+    </div>
+  )
 }
 
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-	<Layout>
-		<SEO title='Home' />
-		<Link to='/terms/'>Go to Terms Page</Link>
-		<br></br>
-		<Link to='/policy/'>Go to Policy Page</Link>
-		<br></br>
-		<Link to='/about/'>Go to About Page</Link>
-	</Layout>
+  <Layout>
+    <SEO title="Home" />
+    <Link to="/terms/">Go to Terms Page</Link>
+    <br></br>
+    <Link to="/policy/">Go to Policy Page</Link>
+    <br></br>
+    <Link to="/about/">Go to About Page</Link>
+  </Layout>
 )
 
 export default IndexPage
